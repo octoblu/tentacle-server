89c3697c30490516e8702315f08c2ad13decfd83
redaphid <a@octoblu.com>
2015-06-22 01:53:32 -0700

    made the loop a little faster

--- examples/message-loop.sh
+++ examples/message-loop.sh
@@ -3,8 +3,8 @@ while :
 do
   echo "turning pin on"
   meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message-on.json ./example-meshblu.json
-  sleep 5
+  sleep 1
   echo "turning pin off"
   meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message-off.json ./example-meshblu.json
-  sleep 5
+  sleep 1
 done
