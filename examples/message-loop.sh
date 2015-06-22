b1d272b3997927a584b671909a5851aaf7250249
redaphid <a@octoblu.com>
2015-06-22 01:42:38 -0700

    added message loop example for stress testing

---
+++ examples/message-loop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while :
+do
+  echo "turning pin on"
+  meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message-on.json ./example-meshblu.json
+  sleep 5
+  echo "turning pin off"
+  meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message-off.json ./example-meshblu.json
+  sleep 5
+done
--- examples/message.sh
+++ examples/message.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message.json ./example-meshblu.json
+meshblu-util message -u b9944342-b8c7-4ca6-9d3e-074eb470626 -f ./message-on.json ./example-meshblu.json
